b0e774ce03c3617c307d133178246b3c4b62374e
Ruico <ruico@Ruico.local>
2018-07-26 12:48:59 +0900

    Add project

---
+++ safetyplus/app/src/main/java/com/example/ruico/safetyplus/gengo.kt
@@ -0,0 +1,12 @@
+package com.example.ruico.safetyplus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class gengo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gengo)
+    }
+}
