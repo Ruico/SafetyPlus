40bd651fef1b61aebed561cb6c921c8dfa0abce6
Ruico <ic151236@edu.okinawa-ct.ac.jp>
2018-07-28 15:41:47 +0900

    Add drive screen, logo

--- safetyplus/app/src/main/java/com/example/ruico/safetyplus/MainActivity.kt
+++ safetyplus/app/src/main/java/com/example/ruico/safetyplus/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +15,19 @@ class MainActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
             val intent = Intent(this, Gengo::class.java)
+            startActivity(intent)
+
+
+        }
+        val button9: Button = findViewById(R.id.button9)
+        button9.setOnClickListener{
+            val intent = Intent( this, Oil_Seeds::class.java)
+            startActivity(intent)
+
+        }
+        val imageButton: ImageButton = findViewById(R.id.imageButton)
+        imageButton.setOnClickListener {
+            val intent = Intent(this, Drive::class.java)
             startActivity(intent)
         }
     }
