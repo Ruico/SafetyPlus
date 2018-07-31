c980d1d0c19418fea153026c5dbf879f16a84de3
Ruico <ic151236@edu.okinawa-ct.ac.jp>
2018-07-31 16:29:59 +0900

    Add image and more

--- safetyplus/app/src/main/java/com/example/ruico/safetyplus/Gengo.kt
+++ safetyplus/app/src/main/java/com/example/ruico/safetyplus/Gengo.kt
@@ -2,11 +2,22 @@ package com.example.ruico.safetyplus
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_gengo.*
 
 class Gengo : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gengo)
+
+        var flag = false
+
+        button7.setOnClickListener {
+            if (flag) {
+                // flagがtrueの時
+                imageView2.setImageResource(R.drawable.drive6)
+                flag = false
+
+        }
     }
 }
