043579e3b94b9e759518606cf4452c0459804caf
karauri14 <tamanaha3141@gmail.com>
2018-08-30 10:36:35 +0900

    rename

--- safetyplus/app/src/main/java/com/example/ruico/safetyplus/Language.kt
+++ safetyplus/app/src/main/java/com/example/ruico/safetyplus/Language.kt
@@ -2,21 +2,22 @@ package com.example.ruico.safetyplus
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_gengo.*
+import kotlinx.android.synthetic.main.activity_language.*
 
-class Gengo : AppCompatActivity() {
+class Language : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gengo)
+        setContentView(R.layout.activity_language)
 
         var flag = false
 
-        button7.setOnClickListener {
+        buttonJp.setOnClickListener {
             if (flag) {
                 // flagがtrueの時
-                imageView2.setImageResource(R.drawable.drive6)
+                checkImage.setImageResource(R.drawable.drive6)
                 flag = false
+            }
 
         }
     }
